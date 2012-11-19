c556c1f7bc7d8b7952a9a09cc4dfcc4748d6d2a6
Rinke Hoekstra <rjhoekstr@gmail.com>
2012-11-19 14:11:03 +0100

    Added option to delete all graphs

--- src/4store/graph_manager.py
+++ src/4store/graph_manager.py
@@ -26,36 +26,63 @@ log.addHandler(logHandler)
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("endpoint", nargs='?', help="The URL of the sparql endpoint (without /sparql suffix)", default='http://eculture2.cs.vu.nl:5020')
+    parser.add_argument("--empty", help="Empty the 4Store model completely", action="store_true")
     args = parser.parse_args()
     
     SPARQL_BASE = args.endpoint
     log.info('Using {} as 4Store location.'.format(SPARQL_BASE))
-    
     sw = SPARQLWrapper('{}/sparql/'.format(SPARQL_BASE))
-    query = "SELECT DISTINCT ?g WHERE { GRAPH ?g {?s ?p ?o}}"
-    
-    sw.setQuery(query)
     sw.setReturnFormat(JSON)
+    sw.addCustomParameter('soft-limit','-1')
     
-    results = sw.query().convert()
-    
-    for res in results["results"]["bindings"] :
-        g = res["g"]["value"]
-        log.info("Graph: <{}>".format(g))
-        
-        yn = raw_input("Delete this graph? (y/n/q): ")
+    if args.empty :
+        yn = raw_input("Are you sure you want to delete all graphs from {} (y/n): ".format(SPARQL_BASE))
         
         if yn == "y" :
-            command = ["curl", "-X", "DELETE", "{}/data/{}".format(SPARQL_BASE,g)]
-            log.info("Deleting graph <{}>".format(g))
-            out = check_output(command)
-            print out
-            log.debug("Done")
-        elif yn == "q":
-            quit()
+            sw.setReturnFormat(JSON)
+            sw.addCustomParameter('soft-limit','-1')
+            
+            q = """SELECT DISTINCT ?g WHERE {
+             GRAPH ?g {
+               ?s ?p ?o .
+             }
+            } """
+            
+            
+            sw.setQuery(q)
+            
+            results = sw.query().convert()
+            
+            for result in results["results"]["bindings"] :
+                graph = result["g"]["value"]
+                log.info("Deleting graph <{}>".format(graph))
         else :
-            log.info("Skipping...")
-    
-    log.info("Finished!")
+            log.info("Quitting...")
+    else :
+        query = "SELECT DISTINCT ?g WHERE { GRAPH ?g {?s ?p ?o}}"
+        
+        sw.setQuery(query)
+        sw.setReturnFormat(JSON)
+        
+        results = sw.query().convert()
+        
+        for res in results["results"]["bindings"] :
+            g = res["g"]["value"]
+            log.info("Graph: <{}>".format(g))
+            
+            yn = raw_input("Delete this graph? (y/n/q): ")
+            
+            if yn == "y" :
+                command = ["curl", "-X", "DELETE", "{}/data/{}".format(SPARQL_BASE,g)]
+                log.info("Deleting graph <{}>".format(g))
+                out = check_output(command)
+                print out
+                log.debug("Done")
+            elif yn == "q":
+                quit()
+            else :
+                log.info("Skipping...")
+        
+        log.info("Finished!")
             
         
